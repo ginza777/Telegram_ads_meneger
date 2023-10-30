7951e48fcfe0af5133273f4e817f9208f2edf7c9
sherzamon <sh.muhiddinov@uic.group>
2023-10-30 12:12:16 +0500

    +

--- Ads_manager/urls.py
+++ Ads_manager/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('system', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 from django.conf import settings
 from django.conf.urls.static import static
