4c08851fc7aecf03a1c19fa1acc731bbd9fdc13f
sherzamon <sh.muhiddinov@uic.group>
2023-11-19 01:18:21 +0500

    new server

--- command.sh
+++ command.sh
@@ -12,11 +12,11 @@ sudo systemctl restart celery_ads_manager
 sudo systemctl restart celery_ads_manager_beat
 
 # Telegram_ads.socket va telegram_ads.service xizmatlarini qayta boshlash
-sudo systemctl restart telegram_ads.socket
-sudo systemctl restart telegram_ads.service
+sudo systemctl restart ads.socket
+sudo systemctl restart ads.service
 
 # client.service xizmatini qayta boshlash (agar kerak bo'lsa)
-sudo systemctl restart client.service
+#sudo systemctl restart client.service
 
 # Nginx konfiguratsiyasini tekshirish va Nginx ni qayta boshlash
 sudo nginx -t && sudo systemctl restart nginx
