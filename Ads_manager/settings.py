31bad82887d0ed57f74c0c0479ad8d8b36894b65
sherzamon <sh.muhiddinov@uic.group>
2023-12-20 12:06:38 +0500

    new server

--- Ads_manager/settings.py
+++ Ads_manager/settings.py
@@ -90,10 +90,10 @@ DATABASES = {
     "default": {
         "ENGINE": env.str("DB_ENGINE"),
         "NAME": env.str("DB_NAME"),
-        "USER": env.str("DB_USER"),
-        "PASSWORD": env.get_value("DB_PASSWORD"),
-        "HOST": env.str("DB_HOST"),
-        "PORT": env.str("DB_PORT"),
+        # "USER": env.str("DB_USER"),
+        # "PASSWORD": env.get_value("DB_PASSWORD"),
+        # "HOST": env.str("DB_HOST"),
+        # "PORT": env.str("DB_PORT"),
 
     }
 }
