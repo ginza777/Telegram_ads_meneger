bcd25a1455dce073e5d3a7b51a821d253d24f48e
JeyJey871 <ginza@tuit.uz>
2024-02-21 16:47:13 +0500

    databse

--- client/management/commands/data.py
+++ client/management/commands/data.py
@@ -18,7 +18,7 @@ def json_loader(filename):
         return json.loads(file.read())
 
 
-def add_data_to_db(data, model):
+def add_data_to_bot_db(data, model):
     for item in data:
         fields = item['fields']
         bot_token = fields.get('bot_token')
@@ -26,7 +26,56 @@ def add_data_to_db(data, model):
             model.objects.create(**fields)
 
 
+def add_data_to_type_db(data, model):
+    for item in data:
+        fields = item['fields']
+        type = fields.get('type')
+        if type and not model.objects.filter(type=type).exists():
+            model.objects.create(**fields)
+
+
+def add_data_to_client_db(data, model):
+    for item in data:
+        fields = item['fields']
+        phone = fields.get('phone')
+        if phone and not model.objects.filter(phone=phone).exists():
+            model.objects.create(**fields)
+
+
+def channel_id_finder(channel_data, id):
+    for item in channel_data:
+        if item['pk'] == id:
+            return item['fields']['channel_id']
+
+
+def type_id_finder(channel_type_data, id):
+    for item in channel_type_data:
+        if item['pk'] == id:
+            return item['fields']['type']
+
+def bot_id_finder(bot_data, id):
+    for item in bot_data:
+        if item['pk'] == id:
+            return item['fields']['bot_token']
+
+
+def add_data_to_keyword_db(data, model):
+    for item in data:
+        fields = item['fields']
+        channel = fields.get('channel')
+        text = fields.get('text')
+
+        channel_id = channel_id_finder(json_loader("channels.json"), channel)
+        channel_obj = Channels.objects.get(channel_id=channel_id)
+        if not model.objects.filter(text=text, channel=channel_obj).exists():
+            model.objects.create(
+                text=text,
+                channel=channel_obj
+            )
+
+
 def add_data_to_channel_db(data, model):
+    print(100 * "*")
     for item in data:
         pk = item['pk']
         channel_name = item['fields'].get('channel_name')
@@ -36,7 +85,10 @@ def add_data_to_channel_db(data, model):
         type = item['fields'].get('type')
         channel_link = item['fields'].get('channel_link')
         channel_id = item['fields'].get('channel_id')
-        type_obj = Channel_type.objects.get(pk=type)
+        type_by_id = type_id_finder(json_loader("channel_type.json"), type)
+        print("type_by_id", type_by_id)
+        type_obj = Channel_type.objects.get(type=type_by_id)
+        print("type_obj", type_obj)
 
         if not model.objects.filter(channel_id=channel_id).exists():
             if my_channel == False:
@@ -49,7 +101,8 @@ def add_data_to_channel_db(data, model):
                     bot=bot,
                     type=type_obj)
             if my_channel == True:
-                bot_obj = Bot.objects.get(pk=bot)
+                bot_token = bot_id_finder(json_loader("bot.json"), bot)
+                bot_obj = Bot.objects.get(bot_token=bot_token)
                 print("my_channel", channel_id, my_channel)
                 model.objects.create(
                     channel_name=channel_name,
@@ -59,24 +112,6 @@ def add_data_to_channel_db(data, model):
                     bot=bot_obj,
                     type=type_obj)
 
-def channel_id_finder(channel_data,id):
-    for item in channel_data:
-        if item['pk'] == id:
-            return item['fields']['channel_id']
-
-def add_data_to_keyword_db(data, model):
-    for item in data:
-        fields = item['fields']
-        channel = fields.get('channel')
-        text = fields.get('text')
-
-        channel_id = channel_id_finder(json_loader("channels.json"),channel)
-        channel_obj= Channels.objects.get(channel_id=channel_id)
-        if not model.objects.filter(text=text, channel=channel_obj).exists():
-            model.objects.create(
-                text=text,
-                channel=channel_obj
-            )
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -87,9 +122,9 @@ class Command(BaseCommand):
             channels_data = json_loader("channels.json")
             keyword_data = json_loader("keyword.json")
 
-            add_data_to_db(bot_data, Bot)
-            add_data_to_db(channel_type_data, Channel_type)
-            add_data_to_db(client_data, Client_Settings)
+            add_data_to_bot_db(bot_data, Bot)
+            add_data_to_type_db(channel_type_data, Channel_type)
+            add_data_to_client_db(client_data, Client_Settings)
             add_data_to_channel_db(channels_data, Channels)
             add_data_to_keyword_db(keyword_data, KeywordChannelAds)
 
