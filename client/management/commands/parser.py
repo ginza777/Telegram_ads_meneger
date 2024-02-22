4ab669e21ad66f9fa6605e611261f19c31ee8db5
JeyJey871 <ginza@tuit.uz>
2024-02-23 00:10:02 +0500

    databse

--- client/management/commands/parser.py
+++ client/management/commands/parser.py
@@ -2,6 +2,8 @@ import requests
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
+from setting_ads.models import Bot
+
 
 def send_to_telegram(bot_token, chat_id):
     caption = f"Ads_manager Date: {timezone.now()}"
@@ -16,6 +18,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         help = 'Starts the Telegram listener'
-        bot_token = "6567332198:AAHRaGT5xLJdsJbWkugqgSJHbPGi8Zr2_ZI"
+        bot_token = Bot.objects.last().bot_token
         chat_id = -1002041724232
         send_to_telegram(bot_token, chat_id)
+        print("done")
