89f5b60121bc4640d65c361f2fdcc4e230a1ca8d
JeyJey871 <ginza@tuit.uz>
2024-02-23 00:19:10 +0500

    databse

--- client/management/commands/parser.py
+++ client/management/commands/parser.py
@@ -3,6 +3,11 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 
 from setting_ads.models import Bot
+def get_info(bot_token):
+    url = f"https://api.telegram.org/bot{bot_token}/getMe"
+    response = requests.post(url)
+    print(response.json())
+    return response.json().get("result").get("username"), response.json().get("result").get("first_name")
 
 
 def send_to_telegram(bot_token, chat_id):
@@ -22,6 +27,9 @@ class Command(BaseCommand):
             bot_token = Bot.objects.last().bot_token
         else:
             bot_token = "6567332198:AAHRaGT5xLJdsJbWkugqgSJHbPGi8Zr2_ZI"
+        print("bot_token: ", bot_token)
+        get_info(bot_token)
         chat_id = -1002041724232
-        send_to_telegram(bot_token, chat_id)
-        print("done")
+        r=send_to_telegram(bot_token, chat_id)
+        print("res:: ", r)
+
